f06f696ac12012909d6e977d34a9beeef3f3b8ff
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-30 15:13:02 +0530

    basic page created and database connection added

---
+++ index.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+const { dbConnection } = require("./db/db.connect");
+
+const errorHandler = require("./middleware/errorHandler");
+const routeHandler = require("./middleware/routeHandler");
+
+dbConnection();
+
+app.get("/", (req, res) => {
+    res.send("One Notion Live...")
+}) 
+
+//middleware
+app.use(routeHandler)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Project running on http://localhost:/${PORT}`);
+})
