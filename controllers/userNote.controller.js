d6ff3ac32ec17cbdc507dd85bd5cb2415a7f2365
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-30 20:05:43 +0530

    getusernote and delete user note function added

--- controllers/userNote.controller.js
+++ controllers/userNote.controller.js
@@ -1,28 +1,88 @@
 const{ UserNote } = require("../models/usernotes.model");
+const { extend } = require("lodash");
+
+const getUserNotes = async(req, res) => {
+    try {
+        const { user } = req;
+        const userNotes = await UserNote.findOne({owner: user.userId});
+        res.json({
+            success: true,
+            userNotes
+        })
+    }
+    catch(err) {
+        res.status(503).json({
+            success: false,
+            message: "No notes created by user"
+        })
+    }
+}
 
 const saveUserNote = async(req, res) => {
     try {
         const { user } = req;
         const { note } = req.body;
-        console.log(user.userId)
-        const newCollection = new UserNote({
-            owner: user.userId,
-            notes: note,
-        })
-        const response = await newCollection.save()
-        res.json({
-            success: true,
-            response
-        })
+
+        const existingCollection = await UserNote.findOne({owner: user.userId});
+        //add to existing notes collection
+        if(existingCollection) {
+            const addNewNote = extend(existingCollection, {
+                notes: [...existingCollection.notes, note]
+            })
+            const newResponse = await addNewNote.save();
+            return res.json({
+                success: true,
+                newResponse
+            })
+        }
+        else {
+            // create new collection for user
+            const newCollection = new UserNote({
+                owner: user.userId,
+                notes: note,
+            })
+            const newResponse = await newCollection.save()
+            res.json({
+                success: true,
+                newResponse
+            })
+        }   
     }
     catch(err) {
-        res.json({
-            success: false,
+        res.status(503).json({ 
+            success: false, 
             message: `Error Occured: ${err}`
         })
     }
 }
 
+const deleteNote = async(req, res) => {
+    const { user } = req;
+    const { noteId } = req.params;
+    try {
+        const userNotes = await UserNote.findOne({owner: user.userId});
+
+        if(userNotes) {
+            const updatedNotesArr = userNotes.notes.filter((noteItem) => noteItem._id != noteId);
+            userNotes.notes = updatedNotesArr;
+
+            const newResponse = await userNotes.save();
+            res.json({
+                success: true,
+                newResponse
+            })
+        }
+    } 
+    catch(err) {
+        res.status(503).json({
+            success:false, 
+            message: `cannot remove from cart ERROR ${err}`
+        })
+    }
+}
+
 module.exports = {
-    saveUserNote
+    getUserNotes,
+    saveUserNote,
+    deleteNote
 }
--- routes/userNote.route.js
+++ routes/userNote.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { saveUserNote } = require("../controllers/userNote.controller");
+const { getUserNotes, saveUserNote, deleteNote } = require("../controllers/userNote.controller");
 const verifyToken = require("../middleware/verifyToken");
 
 router.use(verifyToken)
+router.get("/", getUserNotes);
 router.post("/", saveUserNote)
+router.delete("/:noteId", deleteNote);
 
 module.exports = router;
